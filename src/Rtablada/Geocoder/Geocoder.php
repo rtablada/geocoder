122789d3742afecad157f220bc40d7f3b0ab5950
Ryan Tablada <ryan.tablada@gmail.com>
2013-06-17 19:05:44 -0400

    Added search square

--- src/Rtablada/Geocoder/Geocoder.php
+++ src/Rtablada/Geocoder/Geocoder.php
@@ -35,4 +35,16 @@ class Geocoder
 
 		return $this->location->newInstanceFromObject($response->results[0]);
 	}
+
+	public function getSearchSquare(Coordinate $coordinates, $radius)
+	{
+		$searchSquare = array();
+		$searchSquare['lat'][] = $coordinates->lat - $radius / 69.01;
+		$searchSquare['lat'][] = $coordinates->lat + $radius / 69.01;
+
+		$searchSquare['lng'][] = $coordinates->lng - ($radius / ( 69.172 * cos($coordinates->lat * 0.0174533)));
+		$searchSquare['lng'][] = $coordinates->lng + ($radius / ( 69.172 * cos($coordinates->lat * 0.0174533)));
+
+		return $searchSquare;
+	}
 }
--- tests/Rtablada/Geocoder/GeocoderTest.php
+++ tests/Rtablada/Geocoder/GeocoderTest.php
@@ -14,6 +14,16 @@ class GeocoderTest extends \PHPUnit_Framework_Testcase
 	public function setup()
 	{
 		$this->coordinateResult = new Coordinate(33.74899540, -84.38798240);
+		$this->coordinateSquareResult = array(
+			'lat' => array(
+				33.6765421323576,
+				33.8214486676424
+			),
+			'lng' => array(
+				-84.4749161222352,
+				-84.3010486777648
+			)
+		);
 		$this->locationResult = new Location('Atlanta, GA, USA', $this->coordinateResult);
 
 		$this->curl = m::mock('Curl');
@@ -22,6 +32,34 @@ class GeocoderTest extends \PHPUnit_Framework_Testcase
 		$this->response = new \StdClass;
 		$this->response->body = file_get_contents(__DIR__ . '/json/atlanta.json');
 
+		$this->geocoder = new Geocoder($this->curl, $this->location);
+	}
+
+	public function testGetCoordinatesFromQuery()
+	{
+		$this->setupMocks();
+		$result = $this->geocoder->getCoordinatesFromQuery($this->query);
+
+		$this->assertEquals($this->coordinateResult, $result);
+	}
+
+	public function testGetLocationFromQuery()
+	{
+		$this->setupMocks();
+		$result = $this->geocoder->getLocationFromQuery($this->query);
+
+		$this->assertEquals($this->locationResult, $result);
+	}
+
+	public function testGetSearchSquare()
+	{
+		$result = $this->geocoder->getSearchSquare($this->coordinateResult, 5);
+
+		$this->assertEquals($this->coordinateSquareResult, $result);
+	}
+
+	protected function setupMocks()
+	{
 		$this->curl->shouldReceive('get')
 			->with('http://maps.googleapis.com/maps/api/geocode/json', m::type('Array'))
 			->once()
@@ -31,21 +69,5 @@ class GeocoderTest extends \PHPUnit_Framework_Testcase
 			->with(m::type('StdClass'))
 			->once()
 			->andReturn($this->locationResult);
-
-		$this->geocoder = new Geocoder($this->curl, $this->location);
-	}
-
-	public function testGetCoordinatesFromQuery()
-	{
-		$result = $this->geocoder->getCoordinatesFromQuery($this->query);
-
-		$this->assertEquals($this->coordinateResult, $result);
-	}
-
-	public function testGetLocationFromQuery()
-	{
-		$result = $this->geocoder->getLocationFromQuery($this->query);
-
-		$this->assertEquals($this->locationResult, $result);
 	}
 }
